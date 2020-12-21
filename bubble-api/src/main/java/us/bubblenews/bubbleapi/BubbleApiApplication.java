67d4d7fb71a2191ccab666f31a4f53a0793471b0
Ian Layzer <ianlayzer@gmail.com>
2020-12-21 15:10:43 -0500

    Initialized Spring Boot App

---
+++ bubble-api/src/main/java/us/bubblenews/bubbleapi/BubbleApiApplication.java
@@ -0,0 +1,13 @@
+package us.bubblenews.bubbleapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BubbleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BubbleApiApplication.class, args);
+	}
+
+}
---
+++ bubble-api/src/test/java/us/bubblenews/bubbleapi/BubbleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package us.bubblenews.bubbleapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BubbleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
